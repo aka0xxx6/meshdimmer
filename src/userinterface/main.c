cbbb974674fca4ffd7ba8ba9d22e3f32e6e34636
robin <keller.robin@gmail.com>
2015-02-11 15:12:56 +0100

    user interface with radio communication

--- src/userinterface/main.c
+++ src/userinterface/main.c
@@ -5,13 +5,40 @@
  *      Author: robin
  */
 
+#include "radio/spi.h"
+#include "radio/debug.h"
+#include "radio/dll.h"
+#include "radio/delay_wrapper.h"
 
+#define DLL_ADDRESS 0x20
 
 int main() {
+	uint8_t dll_ret;
+	uint8_t dimmerValue = 0;
 
-	while (1) {
+	DEBUG_init();
+	DEBUG_message((uint8_t *)"debug init done\n", 16);
 
+	sei();
+	spi_init();
+	DEBUG_message((uint8_t *)"spi init done\n", 14);
+
+	dll_ret = DLL_init(DLL_ADDRESS, 15, 21, 0);
+	if (dll_ret != 0) {
+		DEBUG_message((uint8_t *)"dll init failed:", 16);
+		DEBUG_number_hex(dll_ret);
+		DEBUG_newline();
 	}
+
+	while(1) {
+
+		DLL_send(DLL_SEND_TYPE_ACK, 0x30, &dimmerValue, 1);
+		delay(50);
+
+		++dimmerValue;
+	}
+
 
 	return 0;
 }
+
