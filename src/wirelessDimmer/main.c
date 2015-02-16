ce98fd37153353b0d822145609815751b9881558
robin <keller.robin@gmail.com>
2015-02-16 15:04:04 +0100

    add wireless code for receiving adc values

--- src/wirelessDimmer/main.c
+++ src/wirelessDimmer/main.c
@@ -3,7 +3,7 @@
 // Button GND --> PB0
 // Potentimeter--> ADC0
 
-#define F_CPU 1000000UL  // 1MHz internal clock
+//#define F_CPU 1000000UL  // 1MHz internal clock
 //#define fullOn 10
 //#define fullOff 246
 
@@ -12,8 +12,16 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <util/delay.h>
-//#include "lcd.h"
-#include <avr/cpufunc.h>
+
+
+#include "radio/spi.h"
+#include "radio/debug.h"
+#include "radio/dll.h"
+#include "radio/delay_wrapper.h"
+
+//#include <avr/cpufunc.h>
+
+#define DLL_ADDRESS 0x31
 
 uint16_t value;
 uint16_t adcValue;
@@ -41,6 +49,7 @@ ISR(TIMER1_COMPA_vect){
 	TIMSK1 &= ~(1 << OCIE1A); // Disable timer interrupt
 
 	// Wait 10 us, busy wait
+	/*
 	_NOP();
 	_NOP();
 	_NOP();
@@ -51,23 +60,40 @@ ISR(TIMER1_COMPA_vect){
 	_NOP();
 	_NOP();
 	_NOP();
+	*/
+	_delay_us(10);
 
 	PORTD &= ~(1 << PD4); // Stop triac
 }
 
 int main (void)
 {
+	uint8_t dll_ret;
+	uint8_t buffer[DLL_MAX_PACKET_LEN];
+	uint8_t length;
+
+	DEBUG_init();
+	DEBUG_message((uint8_t *)"debug init done\n", 16);
+
+	spi_init();
+	DEBUG_message((uint8_t *)"spi init done\n", 14);
+
+	dll_ret = DLL_init(DLL_ADDRESS, 15, 21, 0);
+	if (dll_ret != 0) {
+			DEBUG_message((uint8_t *)"dll init failed:", 16);
+			DEBUG_number_hex(dll_ret);
+			DEBUG_newline();
+		} else {
+			DEBUG_message((uint8_t *)"DLL init done\n", 14);
+		}
+
 	value = 10;
 	DDRD = 0x10;      //PD4 = Output(Triac), the rest is input
 	initADC0();
 	//lcd_init(LCD_DISP_ON); // initialize LCD
 
-#ifdef EICRA 	// Hack to make Eclipse happy...
 	EICRA |= ( (1<<ISC01) | (1<<ISC00) ); // set INT0 to trigger on RISING edge
 	EIMSK |= (1<<INT0);     // Turns on INT0
-#else
-#error EICRA not defined!
-#endif // End of hack
 
 
 
@@ -84,36 +110,44 @@ int main (void)
 	sei();                    // turn on interrupts
 
 
-  while (1)                         // infinite main loop
-  {
-	  //value = fullOff;
-	  value = 246;
+	while (1)                         // infinite main loop
+	{
+		//value = fullOff;
+		value = 246;
 
-	if((PINB & (1 << 0)) == 0){ //Button pressed
+		if ((PINB & (1 << 0)) == 0) { //Button pressed
+
+			ADCSRA |= (1 << ADSC); /* start ADC conversion */
+			loop_until_bit_is_clear(ADCSRA, ADSC); /* wait until done */
+			adcValue = (ADC / 4.0); /* read ADC in */
+			if (adcValue > 226) {
+				value = 236;
+			} else {
+				value = 10 + adcValue;
+			}
+			//char print[15];
+			//sprintf(print, "%d", value); //int --> char[]
+			//lcd_puts(print);
+
+		}
+		if (DLL_receive(buffer, &length)) {
+			if (length == 2) {
+				adcValue = *((uint16_t*) buffer);
+				adcValue = (adcValue / 4.0); /* read ADC in */
+				if (adcValue > 226) {
+					value = 236;
+				} else {
+					value = 10 + adcValue;
+				}
+			}
+		}
+
+		//_delay_ms(50);                // wait 1000ms between cycles
+		dimtime = 39 * value; //100000 - 10 us / 256 = 39
+		//lcd_clrscr();
 
 
-    	 ADCSRA |= (1 << ADSC); /* start ADC conversion */
-    	 loop_until_bit_is_clear(ADCSRA, ADSC); /* wait until done */
-	     adcValue = (ADC/4.0); /* read ADC in */
-	     	 if(adcValue > 226){value = 236;}
-	     	 else
-	     	 	 {
-	     		 	 value = 10 + adcValue;
-	     	 	 }
-	     	//char print[15];
-	     	//sprintf(print, "%d", value); //int --> char[]
-	     	//lcd_puts(print);
 
-	    }
-
-
-
-
-	    //_delay_ms(50);                // wait 1000ms between cycles
-	    dimtime = 39 * value; //100000 - 10 us / 256 = 39
-	    //lcd_clrscr();
-  }
+	}
 
 }
-
-
