2f5eb345c68f1e6b24490621cb3cda660f01df07
Lars Haulin <lars.haulin@gmail.com>
2015-02-18 17:09:57 +0100

    Added DEBUG_puts() macro

--- src/radio/debug.h
+++ src/radio/debug.h
@@ -2,6 +2,7 @@
 #define DEBUG_H_
 
 #include <inttypes.h>
+#include <string.h>
 
 void DEBUG_init();
 void DEBUG_newline();
@@ -10,6 +11,6 @@ void DEBUG_message(uint8_t * msg, uint8_t len);
 void DEBUG_number(uint16_t u);
 void DEBUG_number_hex(uint8_t u);
 
-
+#define DEBUG_puts(x) do { DEBUG_message((uint8_t *)(x), strlen(x));} while (0)
 
 #endif
