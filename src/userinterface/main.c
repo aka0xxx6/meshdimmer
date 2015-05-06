bf00cc54a7ea93a17c65a50d9e2a96de71c63d07
robin <keller.robin@gmail.com>
2015-05-06 16:28:13 +0200

    remove sending data to light bulb directly

--- src/userinterface/main.c
+++ src/userinterface/main.c
@@ -81,12 +81,9 @@ int main() {
 		adc_value = adc_avarage >> 3;
 
 		if (abs(adc_value - adc_value_old) > 5 ) {
-			uint8_t ret1 = DLL_send(DLL_SEND_TYPE_ACK, 0x30, (uint8_t*)&adc_value, 2);
 			uint8_t ret2 = DLL_send(DLL_SEND_TYPE_ACK, 0x31, (uint8_t*)&adc_value, 2);
 			uint8_t ret3 = DLL_send(DLL_SEND_TYPE_ACK, 0x25, (uint8_t*)&adc_value, 2);
 			DEBUG_number(adc_value);
-			DEBUG_byte(' ');
-			DEBUG_number(ret1);
 			DEBUG_byte(' ');
 			DEBUG_number(ret2);
 			DEBUG_newline();
